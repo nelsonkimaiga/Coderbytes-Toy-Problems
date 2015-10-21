6c34beb1793be6fc44c5566017f4eeab3bab428b
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-10-21 07:17:47 +0300

    replace method for shifting called

--- LetterChallenges/LetterChallenges.java
+++ LetterChallenges/LetterChallenges.java
@@ -1,25 +1,41 @@
-// Using the Java language, have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). 
+// Using the Java language, have the function LetterChanges(str) take the str parameter being passed 
+//and modify it using the following algorithm. 
+//Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). 
 //Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
 
-import java.util.Scanner;
+import java.util.Scanner; 
+import java.io.*;
 
-public class LetterChallenges{
- 
-  public static String capital(String str){
-    String[] newStr = str.split(" ");
-    StringBuffer strbuff = new StringBuffer();
+class Function {  
+ public static String LetterChanges(String str) { 
+  
+    // code goes here   
+    /* Note: In Java the return type of a function and the 
+       parameter types being passed are defined, so this return 
+       call must match the return type of the function.
+       You are free to modify the return type. */
+  StringBuilder shifted = new StringBuilder();
 
-    for(int i = 0; i < newStr.length; i++){
-      strbuff.append(Character.toUpperCase(newStr[i].charAt(0)))
-      .append(newStr[i].substring(1)).append(" ");
-    }
-
-    return strbuff.toString().trim();
+  for (char c : str.toCharArray ()){
+  	if (isLowercase(c)){
+  		//replace
+  		char newChar = (char)((c -'a' + 1) % ('z'-'a') + 'a');
   }
 
 
 
 
 
-
+    return str;
+    
+  } 
+  
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner  s = new Scanner(System.in);
+    Function c = new Function();
+    System.out.print(c.LetterChanges(s.nextLine())); 
+  }   
+  
 }
+

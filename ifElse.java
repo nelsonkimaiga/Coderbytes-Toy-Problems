904164cce54dc6f1e194effdc789495af769ac0f
kimaiga <nelsonkimaiga@aol.com>
2016-01-08 10:46:48 +0300

    if-Else statement

---
+++ ifElse.java
@@ -0,0 +1,11 @@
+public class ifElse{
+	void getText(){
+		text x = x.getText();
+		if (x.getText=="Nelson"){
+			System.out.Println("Cool!");
+		}
+	else{
+System.out.Println("Wrong answer!");
+	}	
+	}
+}

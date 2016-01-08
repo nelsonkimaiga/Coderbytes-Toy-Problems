785058a99f06f32bd76698ff13c9b3906ad70e7f
kimaiga <nelsonkimaiga@aol.com>
2016-01-08 10:51:50 +0300

    Modified if-else statement

--- ifElse.java
+++ ifElse.java
@@ -1,11 +1,14 @@
 public class ifElse{
-	void getText(){
-		text x = x.getText();
-		if (x.getText=="Nelson"){
-			System.out.Println("Cool!");
-		}
-	else{
-System.out.Println("Wrong answer!");
-	}	
-	}
+ public static void main(String[] args) {
+ 	int testscore;
+ 	char grade;
+
+ 	if (testscore >= 90){
+ 		System.out.Println("A");
+ 	} else if (testscore >= 80){
+ 		System.out.Println("B");
+ 	}else if (testscore >= 70) {
+ 		System.out.Println("C");
+ 	}
+ }
 }
